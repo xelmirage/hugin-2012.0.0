61f96ec103a5e8ff50eef1d8f3e1db9a974efc02
xelmirage at xelmirage-pc <xelmirage@pku.edu.cn>
2014-11-15 23:46:41 +0800

    b

--- src/ARLabStitcherwx/pyramidNode.cpp
+++ src/ARLabStitcherwx/pyramidNode.cpp
@@ -20,14 +20,33 @@ bool pyramidNode::writeKml()
 	wxXmlNode lod = wxXmlNode( wxXML_ELEMENT_NODE, wxT("Lod"));
 	region.AddChild(&lod);
 
-	wxString minlodString;
-	minlodString << minLod;
-	wxString maxlodString;
-	maxlodString << maxLod;
-	wxXmlNode minlodpixels = wxXmlNode(wxXML_ELEMENT_NODE, wxT("minLodPixels"),minlodString);
-	wxXmlNode maxlodpixels = wxXmlNode(wxXML_ELEMENT_NODE, wxT("minLodPixels"), minlodString);
 
+	wxXmlNode minlodpixels = wxXmlNode(wxXML_ELEMENT_NODE, wxT("minLodPixels"),boost::lexical_cast<string>(minLod));
+	wxXmlNode maxlodpixels = wxXmlNode(wxXML_ELEMENT_NODE, wxT("minLodPixels"), boost::lexical_cast<string>(maxLod));
+	lod.AddChild(&minlodpixels);
+	lod.AddChild(&maxlodpixels);
 
-	wxXmlNode latlonbox = wxXmlNode(wxXML_ELEMENT_NODE, wxT("LatLonAltBox"), minlodString);
+	wxXmlNode latlonbox = wxXmlNode(wxXML_ELEMENT_NODE, wxT("LatLonAltBox"));
+	region.AddChild(&latlonbox);
+
+	wxXmlNode nodeNorth = wxXmlNode(wxXML_ELEMENT_NODE, wxT("north"), boost::lexical_cast<string>(north));
+	wxXmlNode nodeSouth = wxXmlNode(wxXML_ELEMENT_NODE, wxT("south"), boost::lexical_cast<string>(south));
+	wxXmlNode nodeEast = wxXmlNode(wxXML_ELEMENT_NODE, wxT("east"), boost::lexical_cast<string>(east));
+	wxXmlNode nodeWest = wxXmlNode(wxXML_ELEMENT_NODE, wxT("west"), boost::lexical_cast<string>(west));
+
+	latlonbox.AddChild(&nodeNorth);
+	latlonbox.AddChild(&nodeSouth);
+	latlonbox.AddChild(&nodeEast);
+	latlonbox.AddChild(&nodeWest);
+
+	wxXmlNode groundoverlay = wxXmlNode(wxXML_ELEMENT_NODE, wxT("GroundOverlay"));
+	document.AddChild(&groundoverlay);
+
+	wxXmlNode icon = wxXmlNode(wxXML_ELEMENT_NODE, wxT("Icon"));
+	groundoverlay.AddChild(&icon);
+
+	wxXmlNode href = wxXmlNode(wxXML_ELEMENT_NODE, wxT("href"),image.GetFullName());
+	icon.AddChild(&href);
+
 
 }
