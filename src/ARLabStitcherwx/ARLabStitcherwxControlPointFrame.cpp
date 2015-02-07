91bf121fcdb57336ed0eb384fd16e58731e67242
xelmirage at xelmirage-pc <xelmirage@pku.edu.cn>
2015-02-07 19:17:36 +0800

    a

--- src/ARLabStitcherwx/ARLabStitcherwxControlPointFrame.cpp
+++ src/ARLabStitcherwx/ARLabStitcherwxControlPointFrame.cpp
@@ -8,5 +8,28 @@ ControlPointFrame( parent )
 
 }
 
+void ARLabStitcherwxControlPointFrame::setPTO(wxString s)
+{
+	ptoFile = s;
+}
 
 
+
+int ARLabStitcherwxControlPointFrame::getReady()
+{
+	ifstream prjfile(ptoFile.char_str());
+	if (!prjfile.good()) {
+		wxMessageBox( "could not open script : ");
+		return 1;
+	}
+	pano.setFilePrefix(hugin_utils::getPathPrefix(ptoFile.ToStdString()));
+	DocumentData::ReadWriteError err = pano.readData(prjfile);
+	if (err != DocumentData::SUCCESSFUL) {
+		wxMessageBox("error while parsing panos tool script: " + ptoFile.ToStdString() +"\n"
+			+ "DocumentData::ReadWriteError code: " + lexical_cast<string>(err)+ "\n");
+		return 1;
+	}
+
+
+	return 0;
+}
